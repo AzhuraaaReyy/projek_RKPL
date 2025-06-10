f620e5d7e2fcac488ac7b620848f841bb2548ec9
AzhuraaaReyy <azhurareyy@gmail.com>
2025-06-10 22:13:34 +0700

    BE-40%

--- resources/views/inputProduksiRoti.blade.php
+++ resources/views/inputProduksiRoti.blade.php
@@ -75,7 +75,7 @@
                             </button>
                         </form>
                         @else
-                        <span class="badge bg-success"> ✅</span>
+                        <span class="badge bg-success"> </span>
                         @endif
                     </td>
 
